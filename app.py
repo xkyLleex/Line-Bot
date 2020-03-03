d48d7e073049fa11cc72960c0d044c040a1709af
xkyLleex <leekwanyao710@gmail.com>
2020-03-03 23:18:46 +0800

    kyl

--- app.py
+++ app.py
@@ -12,17 +12,13 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-channel_secret = '13d5c994d2cf69a6dfec00d8c2e38ade'
-channel_access_token = '5qlESxRJZWdeKuRH60FNDJAtPtA96RfMxLtTIliG4qeYUVpKPjTfuJT5rZ92U8fPiM4qmzWT96fnmam7Zf5AFE5KuIt4F9LL9qzEVxnG3sxHLBkA1bxLo9MAfU1N+gOjdi1GcxkYGj9ZxxjFsXJJpwdB04t89/1O/w1cDnyilFU='
+channel_secret = 'SECRET_GET_FROM_HEROKU'
+channel_access_token = 'ACCESS_TOKEN_GET_FROM_HEROKU'
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-@app.route("/")
-def home():
-    return 'home OK'
-
-# 監聽所有來自 /callback 的 Post Request
+    
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -41,13 +37,14 @@ def callback():
 
     return 'OK'
 
-
-# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    input_text = event.message.text
+    input_text = str.lower(input_text)
+    if input_text == "//help":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="input:taipeimrt"))
+    if input_text == "taipeimrt"
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=input_text))
 
 
 if __name__ == "__main__":
